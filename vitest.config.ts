625ce59260e52500b36a0eeebdf7b5955b9c1371
Bruno Mendes <wbruninhoo@gmail.com>
2024-06-21 15:13:34 -0300

    🎉 first commit

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [tsConfigPaths()],
+})
