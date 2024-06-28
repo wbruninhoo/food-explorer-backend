17b374988656d5b2741648f9b39865ce419d73f0
Bruno Mendes <wbruninhoo@gmail.com>
2024-06-28 20:52:28 -0300

    feat: add conditional server host setup based on deployment environment

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,4 +1,8 @@
 import { env } from '../env'
 import { app } from './app'
 
-app.listen(env.PORT, () => console.log('🚀 HTTP server running.'))
+const port = env.PORT || 3000
+app.listen(port, () => {
+  const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+  console.log(`🚀 HTTP server running on ${host}:${port}`)
+})
